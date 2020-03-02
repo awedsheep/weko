0e8cfbe116637313ff6735ba3119a911df90b96f
KJ <awedsheep@gmail.com>
2020-03-01 21:16:50 -0600

    prevent duplicate api call at home

--- src/components/HomeBoard/Board.js
+++ src/components/HomeBoard/Board.js
@@ -4,62 +4,64 @@ import "./Board.css";
 import more from "../images/more.png";
 import { Link } from "react-router-dom";
 import { getRecentTwenty } from "../../apiCall";
-import { useGlobalState } from "../../state"
+import { useGlobalState } from "../../state";
 import { Icon } from "semantic-ui-react";
 
-function Board({ newestID, name, cat, data }) {
-
+function Board({ name, cat }) {
 	const [postings, setPostings] = useGlobalState(cat);
-	const [loaded, setLoaded] = useState(false);
 
+	var initLoad = false;
+	if (postings.length !== 0) {
+		initLoad = true;
+	}
+	const [loaded, setLoaded] = useState(initLoad);
 	// console.log(cat)
 	async function fetchTenItems() {
 		var res;
-		if (cat === "forum") {
-			res = await getRecentTwenty("entertainment");
-			setPostings(res);
-			setLoaded(true);
-		} else if (cat === "news") {
-			res = await getRecentTwenty("news");
-			setPostings(res);
-			setLoaded(true);
+		if (postings.length === 0) {
+			if (cat === "forum") {
+				res = await getRecentTwenty("entertainment");
+				setPostings(res);
+				setLoaded(true);
+			} else if (cat === "news") {
+				res = await getRecentTwenty("news");
+				setPostings(res);
+				setLoaded(true);
+			}
 		}
-
 	}
 	// console.log(data);
-	
+
 	useEffect(() => {
 		fetchTenItems();
 	}, []);
 
 	// console.log(tenItems);
 	// console.log(postings);
-	var placeholder = new Array(10).fill(0) //soreum
+	var placeholder = new Array(10).fill(0); //soreum
 	return (
 		<div className="board">
 			<div className="board_header">
-				<Link to={"/" + cat+"/1"} className="board__title">
+				<Link to={"/" + cat + "/1"} className="board__title">
 					{name}
 				</Link>
-				<Link to={"/" + cat+"/1"} className="board__more">
+				<Link to={"/" + cat + "/1"} className="board__more">
 					<img src={more} alt="more" />
 				</Link>
 			</div>
 			<div className="board_list">
 				<ul>
-					{loaded ?
-						postings.slice(0,10).map((item, i) => {
-							return (<BoardItem
-								key={i}
-								num={i}
-								item={item}
-								cat={cat}
-							/>);
-
-						}) : placeholder.map(element => {
-							return <li><Icon loading name='spinner'/></li>
-						})
-					}
+					{loaded
+						? postings.slice(0, 10).map((item, i) => {
+								return <BoardItem key={i} num={i} item={item} cat={cat} />;
+						  })
+						: placeholder.map(element => {
+								return (
+									<li>
+										<Icon loading name="spinner" />
+									</li>
+								);
+						  })}
 				</ul>
 			</div>
 		</div>
--- src/components/HomeBoard/BoardsWrap.js
+++ src/components/HomeBoard/BoardsWrap.js
@@ -2,24 +2,14 @@ import React from "react";
 import Board from "./Board";
 import "./BoardsWrap.css";
 
-
-
-let res;
-function BoardsWrap({ data }) {
-	
-	
+function BoardsWrap() {
 	return (
 		<div className="boarder_warp">
 			<div>
-				<Board name="캐나다소식" newestID={res} cat="news" data={data.news} />
-				<Board name="자유게시판" newestID={res} cat="forum" data={data.forum} />
-				<Board
-					name="온라인장터"
-					newestID={res}
-					cat="buysell"
-					data={data.buysell}
-				/>
-				<Board name="정보/팁" newestID={res} cat="info" data={data.info} />
+				<Board name="캐나다소식" cat="news" />
+				<Board name="자유게시판" cat="forum" />
+				<Board name="온라인장터" cat="buysell" />
+				<Board name="정보/팁" cat="info" />
 			</div>
 		</div>
 	);
